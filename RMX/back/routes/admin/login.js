4c1692ce5ccc77d252825f0994b070d47479824f
Gonzalo Lezama <gonzalo@lezamapc.com.ar>
2024-11-02 01:23:41 -0300

    M5unidad4

--- RMX/back/routes/admin/login.js
+++ RMX/back/routes/admin/login.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin/login' ,{ //login.hbs
-    layout:'admin/layout' //admin/layout.hbs
+router.get('/', function (req, res, next) {
+  res.render('admin/login', { //login.hbs
+    layout: 'admin/layout', //admin/layout.hbs
+  });
+});
+
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.render('admin/login', {
+    layout: 'admin/layout'
+  });
+});
+
+router.post('/', async (req, res, nest) => {
+  try {
+    var usuario = req.body.usuario; //captura la info //
+    var password = req.body.password; //1234//
+
+    var data = await
+      usuariosModel.getUserByUsuernameAndPassword(usuario, password);
+
+    if (data != undefined) {
+
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario;
+
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.render('admin/login', {
+    layout: 'admin/layout'
   });
 });
 
